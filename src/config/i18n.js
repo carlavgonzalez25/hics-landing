a9256c4374b2f71994fbe8bf5fab0dba935d674e
Unknown <matiassanchezrivas@gmail.com>
2020-04-12 02:29:58 +1000

    Adds i18n

---
+++ src/config/i18n.js
@@ -0,0 +1,20 @@
+export default {
+  resources: {
+    en: {
+      translation: {
+        office: 'office',
+      },
+    },
+    es: {
+      translation: {
+        office: 'Oficina',
+      },
+    },
+  },
+  lng: 'en',
+  fallbackLng: 'en',
+
+  interpolation: {
+    escapeValue: false,
+  },
+}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import App from './App'
 import { configureStore } from './redux/store'
 import { Provider } from 'react-redux'
 import * as serviceWorker from './serviceWorker'
+import i18n from 'i18n'
+import { initReactI18next } from 'react-i18next'
+import i18nInit from './config/i18n'
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init(i18nInit)
 
 ReactDOM.render(
   <React.StrictMode>
